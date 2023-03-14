713f9a158d85871ff70b1554edd1d1e1ca78b40c
solmazkadir <abdlkadirsolmaz@gmail.com>
2023-03-14 23:01:22 +0300

    Add project files.

---
+++ P013AspNetMVCEgitim/Controllers/MVC03DataTransferController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P013AspNetMVCEgitim.Controllers
+{
+    public class MVC03DataTransferController : Controller
+    {
+        public IActionResult Index(string Ara) //get metoduyla gönderilen verileri bu şekilde Ara yazarak yakalayabiliriz
+        {
+            //Mvc de temel olarak 3 farklı yöntemle ekrana veri gönderebiliriz
+
+            //1- ViewBag : Tek kullanımlık ömrü var
+            ViewBag.UrunKategorisi = "Bilgisayar";
+            //2- ViewData : Tek kullanımlık ömrü var
+            ViewData["UrunAdi"] = "Asus Dizüstü Bilgisayar";
+            //3- TempData : İki kullanımlık ömrü var
+            TempData["UrunFiyati"] = 9999;
+            ViewBag.GetVerisi = Ara;
+            return View();
+        }
+        [HttpPost] //aşağıdaki metot post işlemide çalışsın
+        public IActionResult Index(string text1,string ddlListe, bool cbOnay)
+        {
+            ViewBag.BirinciYontem = "1.Yöntemle(Parametrelerden gelen veriler)";
+            ViewBag.Mesaj = "Textboxtan gelen veri : " +text1;
+            ViewBag.MesajListe = "ddlListeden gelen veri : " +ddlListe;
+            TempData["Tdata"] = "cbOnaydan gelen değer : " + cbOnay;
+            return View();
+        }
+    }
+}
---
+++ P013AspNetMVCEgitim/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection(); //uygulama http isteklerini https ile güvenli baðlantýya yönlendirmeyi desteklesin
+app.UseStaticFiles(); //uygulama statik dosyalarý(wwwroot klasöründekiler) desteklesin
+
+app.UseRouting(); //uygulama routing ile yönlendirmeyi kullansýn
+
+app.UseAuthentication(); //uygulam kullanýcý giriþ sistemini aktif etsin
+app.UseAuthorization(); //uygulama kullanýcý yetkilendime(admin, user) sistemini aktif etsin
+
+app.MapControllerRoute( //uygulamanýn kullanacaðý routing yönlendirme ayarý
+    name: "default", //route adý
+    pattern: "{controller=Home}/{action=Index}/{id?}"); // eðer adres çubuðundan uygulama bir controller adý ve action adý gönderilmezse varsayýlan olarak Home controlleri ve içindeki index isimli actioný çalýþtýrsýn. Id deðeri ise ? iþareti ile parametrik yani isteðe baðlý belirtilmiþ
+
+app.Run(); //uygulamayý yukarýdaki ayarlarý kullanarak çalýþtýr
